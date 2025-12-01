b33e2861ee4ddce90318da03166b9ba8a54a6fac
JamesLau2001 <james_lau2001@outlook.com>
2025-12-01 13:50:40 +0000

    Attempt to deploy

--- main.tf
+++ main.tf
@@ -1,31 +1,42 @@
 provider "aws" {
-  region = "eu-west-2"
+    region = "eu-north-1"
 }
 
- data "aws_ami" "ubuntu" {
-  most_recent = true
-
-  filter {
-    name = "name"
-    values = ["ubuntu/images/hvm-ssd-gp3/ubuntu-noble-24.04-amd64-server-*"]
-  }
-
-  owners = ["099720109477"] 
+data "aws_ami" "ubuntu" {
+    most_recent = true
+    filter {
+        name = "name"
+        values = ["ubuntu/images/hvm-ssd-gp3/ubuntu-noble-24.04-amd64-server-*"]
+    }
+    owners = ["099720109477"]
 }
 
-resource "aws_vpc" "main" {
-  cidr_block = "10.0.0.0/16"
+resource "tls_private_key" "ssh_key" {
+  algorithm = "RSA"
+  rsa_bits  = 4096
+}
+
+resource "local_file" "private_key" {
+  content  = tls_private_key.ssh_key.private_key_pem
+  filename = "./.ssh/terraform_rsa"
+}
+
+resource "local_file" "public_key" {
+  content  = tls_private_key.ssh_key.public_key_openssh
+  filename = "./.ssh/terraform_rsa.pub"
+}
+
+resource "aws_instance" "app_server" {
+  ami                    = data.aws_ami.ubuntu.id
+  instance_type          = "t3.micro"
+  subnet_id              = "subnet-84e2f4fc"
+  vpc_security_group_ids = ["sg-00d5a1d67232f43a4"]
+  key_name               = "testing_pyramid_key"
+  user_data = templatefile("${path.module}/cloud-init.yaml.tmpl", {
+    instance_name = "james_lau_testing_pyramid"
+  })
 
   tags = {
-    Name = "testing-vpc"
-  }
-}
-
-resource "aws_instance" "testing-pyramid" {
-  ami           = data.aws_ami.ubuntu.id
-  instance_type = "t2.micro"
-
-  tags = {
-    Name = "testing-pyramid"
+    Name = "james_lau_testing_pyramid"
   }
 }
