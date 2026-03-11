3ee8b02ca918721d5f7f92465b5d7f23bbb5da7f
JamesLau2001 <james_lau2001@outlook.com>
2026-03-11 14:00:23 +0000

    deploy

--- cypress.config.js
+++ cypress.config.js
@@ -3,7 +3,7 @@ const { defineConfig } = require('cypress');
 module.exports = defineConfig({
   e2e: {
     baseUrl: 'http://localhost:3000',
-    defaultCommandTimeout: 8000,
+    defaultCommandTimeout: 20000,
     pageLoadTimeout: 30000,
     uncaughtExceptions: false,
   },
